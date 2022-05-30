c393ed53937e05d31326e0c56f8a113efa732798
MxNl <maxnoelscher@posteo.de>
2022-05-30 23:44:39 +0200

    first datatable in index.html

--- R/awesome_geodata_table_html.R
+++ R/awesome_geodata_table_html.R
@@ -1,7 +1,16 @@
 awesome_geodata_table_html <- function(x) {
   DT::datatable(x,
-                filter = 'top',
-                options = list(
-    pageLength = 5, autoWidth = TRUE
-  ))
+    filter = "top",
+    options = list(
+      pageLength = 5,
+      # autoWidth = TRUE,
+      width = '100%',
+      scrollX = TRUE,
+      # scrollY = TRUE,
+      paging = FALSE,
+      scrollY = '50vh',
+      scrollCollapse = TRUE
+    )
+  ) |>
+    DT::formatStyle(columns = 1:ncol(x), fontSize = '90%')
 }
