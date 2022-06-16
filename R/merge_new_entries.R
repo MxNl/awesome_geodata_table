65b0a4f42100874bf7dbf4645b459c003a9b88a5
MxNl <maxnoelscher@posteo.de>
2022-06-16 16:28:15 +0200

    type_convert for txt files

---
+++ R/merge_new_entries.R
@@ -0,0 +1,20 @@
+merge_new_entries <- function (filepath) {
+  table_old <- CSV_TABLE_PATH %>%
+    awesome_geodata_table_read()
+
+  unique_header_clean <- CSV_TABLE_PATH %>%
+    complex_header() %>%
+    unique_headers_clean() %>%
+    pull(column_names)
+
+  table_old <- table_old %>%
+    set_names(unique_header_clean)
+
+  new_entries <- "inst/extdata/new_entries" %>%
+    list.files(full.names = TRUE, pattern = "*.txt") %>%
+    map_df(read_new_entry) %>%
+    readr::type_convert(col_types = read_column_types(CSV_TABLE_PATH))
+
+  table_old %>%
+    bind_rows(new_entries)
+}
