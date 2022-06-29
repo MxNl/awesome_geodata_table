92d64dd3700ca32ae81743e4706fd56bd5fd33e4
MxNl <maxnoelscher@posteo.de>
2022-06-29 18:25:34 +0200

    basically implemented html table with reactable package; download csv not properly formatted

--- R/awesome_geodata_table_read.R
+++ R/awesome_geodata_table_read.R
@@ -11,5 +11,6 @@ awesome_geodata_table_read <- function(filepath) {
   readr::read_csv2(
     filepath,
     col_types = coltypes,
-    skip = 3)
+    skip = 2,
+    locale = readr::locale(encoding = "ISO-8859-1"))
 }
--- R/get_table_header_csv.R
+++ R/get_table_header_csv.R
@@ -2,7 +2,7 @@ get_table_header_csv <- function() {
   CSV_TABLE_PATH |>
     readr::read_csv2(
       col_names = FALSE,
-      n_max = 4,
+      n_max = 3,
       show_col_types = FALSE, locale = readr::locale(decimal_mark = ",", grouping_mark = ".")
     )
 }
