857cfdc300a9f8e6bc843243a7efbac02ca6a2d0
MxNl <maxnoelscher@posteo.de>
2022-08-09 14:21:50 +0200

    shiny intermediate state

--- R/app.R
+++ R/app.R
@@ -13,7 +13,7 @@ library(katexR)
 
 # data("COLUMN_CATEGORIES_TEMPRES")
 
-list.files("R", full.names = TRUE) %>%
+list.files("", full.names = TRUE) %>%
   discard(str_detect, "app.R") %>%
   map(source)
 
@@ -34,11 +34,16 @@ temporal_res_unique <- COLUMN_CATEGORIES_TEMPRES %>% discard(str_detect, "static
 
 awesome_geodata_table_App <- function() {
 
-
   ui = tagList(
     navbarPage(
       # theme = "cerulean",  # <--- To use a theme, uncomment this
-      "",
+      "AwesomeGeodataTable",
+      # titlePanel(
+      #   setBackgroundImage(
+      #     src = "../earth_black_background.png"
+      #   )
+      # ),
+      header = tags$img(src = 'https://images.unsplash.com/photo-1582641857491-7e9987c2e415?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', width = '100%'),
       tabPanel("Awesome Geodata Table",
                sidebarPanel(
                  awesometableFilterUI(
